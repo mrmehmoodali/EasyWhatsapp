2c42bc13d66b05158b7d8f3886da653cb6b282d8
Ali <zeuslabtechsolutions@gmail.com>
2018-01-28 22:32:35 +0530

    Added country code picker

--- app/src/main/java/com/zeus/ali/easywhatsapp/MainActivity.java
+++ app/src/main/java/com/zeus/ali/easywhatsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.zeus.ali.easywhatsapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Build;
 
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +19,21 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.hbb20.CountryCodePicker;
+
 import es.dmoral.toasty.Toasty;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     ImageButton infobutton;
     Button button;
     EditText phone;
     EditText messageText;
     private String urlLink;
     String phvalue, msvalue;
+    CountryCodePicker ccp;
 
 
     @Override
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         infobutton = findViewById(R.id.imageButton);
         phone = findViewById(R.id.phone);
         messageText = findViewById(R.id.messageText);
+        ccp = findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(phone);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -55,9 +63,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                phvalue = phone.getText().toString();
+
+                phvalue = ccp.getFullNumber();
                 msvalue = messageText.getText().toString();
                 urlLink = "https://api.whatsapp.com/send?phone="+phvalue+"&text="+msvalue;
+
                 //Log.e(TAG, "whatsappurl " + urlLink);
                 if (TextUtils.isEmpty(phone.getText())) {
                     Toasty.error(getApplicationContext(),
@@ -79,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Toasty.info(getApplicationContext(),
-                        "Add the phone number in international format, omitting any zeroes, brackets or dashes",
+                        "Choose country code, type phone number & message and click whatsapp. Simple!",
                         Toast.LENGTH_LONG, true).show();
             }
         });
